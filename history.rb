039e4194361efe78593aaa31ff235293c51f928d
b4b4r07 <b4b4r07@gmail.com>
2017-06-13 18:42:47 +0900

    Improve binaries

--- history.rb
+++ history.rb
@@ -3,11 +3,11 @@ class History < Formula
   version '0.0.1'
 
   if Hardware::CPU.is_64_bit?
-    url 'https://github.com/b4b4r07/history/releases/download/0.0.1/darwin_amd64_history'
-    sha256 'bf3a57b076ca79c16a6ba1838f4633521292fd6531ce29b3bf87f77baff33470'
+    url 'https://github.com/b4b4r07/history/releases/download/0.0.1/history-0.0.1-darwin-amd64.tar.gz'
+    sha256 'b8b72b91978df3f1b2f5177ad8ce0194ab096c5d9c7acdfd4c26fcd25ab1ca47'
   else
-      # TODO
-      return
+    url 'https://github.com/b4b4r07/history/releases/download/0.0.1/history-0.0.1-darwin-386.tar.gz'
+    sha256 '9a71555b8288164469dcdcde4260ff4c247d885f695e44a6b068e9005653cd9d'
   end
 
   head do
@@ -23,9 +23,9 @@ class History < Formula
       mkdir_p buildpath/'.go/src/github.com/b4b4r07'
       ln_s buildpath, buildpath/'.go/src/github.com/b4b4r07/history'
       system 'make', 'build'
-      bin.install 'build/history'
+      bin.install 'bin/history'
     else
-      bin.install 'darwin_amd64_history' 'history'
+      bin.install 'history'
     end
   end
 
